381212da40fe538e688163bb63b3bafb2c3a132b
Chris <Drabsch>
2012-09-04 00:34:16 +0930

    Testing commit

---
+++ fork-update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Updates the repository with changes from the default template repo
+# Based on "Pull in upstream changes": https://help.github.com/articles/fork-a-repo
+
+echo "Fetching upstream changes..."
+sleep 1
+git fetch upstream
+echo "Merging changes..."
+sleep 1
+git merge upstream/master
